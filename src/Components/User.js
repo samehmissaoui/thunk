0923aa4fa09fc12d574f766f00bc83b9c2b4b123
sameh <sameh.missaoui2505@gmail.com>
2023-11-27 21:53:23 +0100

    first commit

---
+++ src/Components/User.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { fetchUsers } from '../Js/action'
+
+function Users ({ userData, fetchUsers }) {
+  useEffect(() => {
+    console.log(userData);
+    fetchUsers()
+  }, [fetchUsers])
+  return userData.loading ? (
+    <h2>Loading</h2>
+  ) : userData.error ? (
+    <h2>{userData.error}</h2>
+  ) : (
+    <div>
+      <h2>Users List</h2>
+      <div>
+        {userData &&
+          userData.users &&
+          userData.users.map(user => <p>{user.name}</p>)}
+      </div>
+    </div>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    userData: state
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchUsers: () => dispatch(fetchUsers())
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Users)
